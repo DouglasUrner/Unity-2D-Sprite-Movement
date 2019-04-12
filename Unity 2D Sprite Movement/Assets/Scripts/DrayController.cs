b7af079f6a72376f6a74d52320b936e7950f53c9
Douglas Urner <dlu@canishe.com>
2019-04-12 06:10:49 -0700

    Fixed indents, made sprite name visible in inspector.

--- Unity 2D Sprite Movement/Assets/Scripts/DrayController.cs
+++ Unity 2D Sprite Movement/Assets/Scripts/DrayController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class DrayController : MonoBehaviour
 {
+  public string spriteBaseName = "Dray_Walk_";
   public float speed = 5;
 
   public int dir = -1; // Direction of held movement key.
@@ -11,34 +12,34 @@ public class DrayController : MonoBehaviour
   private Rigidbody rb;
   private Animator anim;
 
-    // Start() is called once before the first frame.
-    void Start()
-    {
-        rb = GetComponent<Rigidbody>();
-        anim = GetComponent<Animator>();
+  // Start() is called once before the first frame.
+  void Start()
+  {
+    rb = GetComponent<Rigidbody>();
+    anim = GetComponent<Animator>();
+  }
+
+  // Update() is called once per frame
+  void Update()
+  {
+    Vector3[] vels = {Vector3.right, Vector3.up, Vector3.left, Vector3.down};
+    var vel = Vector3.zero;
+
+    anim.speed = 0; // Speed the animator runs at.
+
+    dir = -1;
+
+    if (Input.GetKey(KeyCode.RightArrow)) {dir = 0;}
+    if (Input.GetKey(KeyCode.UpArrow)) {dir = 1;}
+    if (Input.GetKey(KeyCode.LeftArrow)) {dir = 2;}
+    if (Input.GetKey(KeyCode.DownArrow)) {dir = 3;}
+
+    if (dir != -1) {
+      vel = vels[dir];
+      anim.CrossFade(spriteBaseName + dir, 0);
+      anim.speed = 1;
     }
 
-    // Update() is called once per frame
-    void Update()
-    {
-      Vector3[] vels = {Vector3.right, Vector3.up, Vector3.left, Vector3.down};
-      var vel = Vector3.zero;
-
-      anim.speed = 0; // Speed the animator runs at.
-
-      dir = -1;
-
-      if (Input.GetKey(KeyCode.RightArrow)) {dir = 0;}
-      if (Input.GetKey(KeyCode.UpArrow)) {dir = 1;}
-      if (Input.GetKey(KeyCode.LeftArrow)) {dir = 2;}
-      if (Input.GetKey(KeyCode.DownArrow)) {dir = 3;}
-
-      if (dir != -1) {
-        vel = vels[dir];
-        anim.CrossFade("Dray_Walk_" + dir, 0);
-        anim.speed = 1;
-      }
-
-      rb.velocity = vel * speed;
-    }
+    rb.velocity = vel * speed;
+  }
 }
